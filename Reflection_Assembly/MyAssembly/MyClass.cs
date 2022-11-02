9c263ea06d50af4d5e834f7ee729c6d54b265c6a
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-11-02 22:52:12 +0530

    Reflection
    Assignment 8.1

---
+++ Reflection_Assembly/MyAssembly/MyClass.cs
@@ -0,0 +1,32 @@
+﻿namespace MyAssembly
+{
+   
+    public class MyClass
+    {
+        int X { get; set; }
+        public int Y { get; set; }
+
+        public MyClass(int initialX, int initialY)
+        {
+          X = initialX;
+          Y = initialY;
+        }
+
+        public int MyMethod(int count, string text)
+        {
+            Console.WriteLine("simple method ");
+            Console.WriteLine("Count: {0}", count);
+            Console.WriteLine("Text: {0}", text);
+
+            return this.X + this.Y;
+        }
+
+        public static void StaticMethod(int count, float radius)
+        {
+            Console.WriteLine("this is static method");
+            Console.WriteLine("Count: {0}", count);
+            Console.WriteLine("Radius: {0}", radius);
+        }
+
+    }
+}
---
+++ Reflection_Assembly/Reflection_Assembly/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+
+
+namespace Reflection_Assembly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Assembly
+
+            try
+            {
+                Assembly asm = Assembly.LoadFrom(@"C:\Users\akshata\source\repos\Reflection_Assembly\MyAssembly\bin\Debug\net6.0\MyAssembly.dll");
+                Type t = asm.GetType("MyAssembly.MyClass");
+
+
+                var methodInfo = t.GetMethod("MyMethod", new Type[] { typeof(int), typeof(string) });
+                if (methodInfo == null)
+                {
+
+                    throw new Exception("MethodnotFound");
+                }
+
+
+                object[] constructorParameters = new object[2];
+                constructorParameters[0] = 100;
+                constructorParameters[1] = 100;
+
+
+                var o = Activator.CreateInstance(t, constructorParameters);
+
+
+                object[] parameters = new object[2];
+                parameters[0] = 26;
+                parameters[1] = "this is a text";
+
+
+                var r = methodInfo.Invoke(o, parameters);
+                Console.WriteLine(r);
+
+                var methodInfoStatic = t.GetMethod("StaticMethod");
+                if (methodInfoStatic == null)
+                {
+
+                    throw new Exception("InvalidMethoodCallException");
+                }
+
+
+                object[] staticParameters = new object[2];
+                staticParameters[0] = 12;
+                staticParameters[1] = 3.14159f;
+
+
+                methodInfoStatic.Invoke(o, staticParameters);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error Message"+e.Message);
+            }
+            
+
+            #endregion
+
+
+
+        }
+    }
+}
