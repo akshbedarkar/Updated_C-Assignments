38f298f03a74cbeae9ca3c9cd2541a5144d187b2
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-11-02 22:58:02 +0530

    Deserialization of employee class
    assignments 7.3 and 7.4

---
+++ Serialization/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //string path = @"C:\Users\akshata\source\fieio\MyDirectory\MyAnotherDirectory\NewFile.txt";
+                //employee emp = new employee(101,"akshata",20000);
+                //FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+                //BinaryFormatter bf=new BinaryFormatter();
+                //bf.Serialize(fs,emp);
+                //fs.Close();
+
+
+
+                string path = @"C:\Users\akshata\source\fieio\MyDirectory\MyAnotherDirectory\NewFile.txt";
+                FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+                BinaryFormatter bf = new BinaryFormatter();
+                employee emp = (employee)bf.Deserialize(fs);
+                Console.WriteLine("employee id : " + emp.id);
+                Console.WriteLine("employee Name : " + emp.name);
+                Console.WriteLine("employee basic salary : " + emp.basicsalary);
+
+                fs.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error Message"+e.Message);
+            }
+
+            
+        }
+    }
+}
---
+++ Serialization/employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    internal class employee
+    {
+        #region VariableDeclaration
+
+        
+        public int id;
+        public string name;
+        public int basicsalary;
+        #endregion
+
+        #region constructor
+
+        public employee(int id, string name, int basicsalary)
+        {
+            this.id = id;
+            this.name = name;
+            this.basicsalary = basicsalary;
+        }
+        #endregion
+
+
+    }
+}
