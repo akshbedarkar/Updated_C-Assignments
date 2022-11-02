1493c7f3a76150c558000fd0e7aa156a286a902e
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-11-02 22:13:32 +0530

    SalaryCalculationSystem
    assignment 2

---
+++ EmployeeManagementSystem/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    public class EmployeePer
+    {
+
+        static void Main(String[] args)
+        {
+            var emp1 = new ArrayList();
+            var emp2 = new ArrayList();
+            var emp3 = new ArrayList();
+        }
+    }
+}
---
+++ EmployeeManagementSystem/Program.cs
@@ -0,0 +1,149 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace EmployeeManagementSystem
+{
+    public class Employee
+    {
+        #region variableDeclaration
+
+        
+        private double Eno;
+        private string EmpName;
+        private double salary;
+        private double HRA;
+        private double TA;
+        private double DA;
+        private double PF;
+        private double TDS;
+        private double NetSalary;
+        private double GrossSalary;
+        #endregion
+        #region constantDeclaration
+
+        
+        const long firstsalaryslap = 5000;
+        const long secondsalaryslap = 10000;
+        const double thirdsalaryslap = 15000;
+        const double fourthsalaryslap = 20000;
+        #endregion
+
+        #region method
+
+        
+        public void grosssalary()
+        {
+            this.GrossSalary = this.salary + this.HRA + this.TA + this.DA;
+        }
+
+        //method to calculate salary
+        public void calculatesalary()
+        {
+            this.PF = (this.GrossSalary * 0.1);
+            this.TDS = (this.GrossSalary * 0.18); ;
+            this.NetSalary = (this.GrossSalary - (this.PF + this.TDS));
+        }
+
+        public void displaysalary()
+        {
+            Console.WriteLine($"Details of {EmpName} \n Employee Id :{Eno} \n Employee Salary {salary} \n HRA {HRA}  \n TA :{TA} \n DA:{DA} \n PF : {PF} \n TDS :{TDS} \n Net Salary {NetSalary} \n Gross Salary : {GrossSalary}");
+        }
+
+        public void calculation()
+        {
+            if (salary <= 0 )
+            {
+                throw new Exception("ZeroOrNegativeSalaryExceptioin");
+            }
+            else
+            {
+                if (salary < firstsalaryslap)
+                {
+                    this.HRA = (salary * 0.1);
+                    this.TA = (salary * 0.05);
+                    this.DA = (salary * 0.15);
+                    grosssalary();
+                }
+                else if (salary < secondsalaryslap)
+                {
+                    this.HRA = (salary * 0.15);
+                    this.TA = (salary * 0.1);
+                    this.DA = (salary * 0.20);
+                    grosssalary();
+                }
+                else if (salary < thirdsalaryslap)
+                {
+                    this.HRA = (salary * 0.20);
+                    this.TA = (salary * 0.15);
+                    this.DA = (salary * 0.25);
+                    grosssalary();
+                }
+                else if (salary < fourthsalaryslap)
+                {
+                    this.HRA = (salary * 0.25);
+                    this.TA = (salary * 0.20);
+                    this.DA = (salary * 0.30);
+                    grosssalary();
+                }
+                else
+                {
+                    this.HRA = (salary * 0.30);
+                    this.TA = (salary * 0.25);
+                    this.DA = (salary * 0.35);
+                    grosssalary();
+                }
+            }
+        }
+        #endregion
+        #region constructor
+
+        
+        public Employee(double eno,String ename, double salary)
+        {
+           
+            
+            this.Eno = eno;
+            this.EmpName = ename;
+            this.salary = salary;
+
+            calculation();
+
+        }
+        #endregion
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to Employee Application");
+
+                Console.WriteLine("Enter the Employee Number");
+                double eno = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Enter the Employee Name");
+                string ename = Console.ReadLine();
+
+
+                Console.WriteLine("Enter the Employee Salary");
+                double salary = int.Parse(Console.ReadLine());
+
+
+                Employee employee1 = new Employee(eno, ename, salary);
+                employee1.calculatesalary();
+                employee1.displaysalary();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error Message : "+e.Message);
+            }
+            
+           
+        }
+            
+
+        
+
+    }
+}
+
+
