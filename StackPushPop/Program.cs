43b6b5df07737ee67e03edc248e792caef02f27c
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-11-02 23:05:55 +0530

    Handling Exception with Stack
    Assignment 3.3

---
+++ StackPushPop/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Drawing;
+
+namespace StackPushPop
+{
+    public class StackException: Exception
+    {
+        public StackException(String message)
+            : base(message)
+        {
+
+        }
+    }
+    public class Mystack 
+    {
+        #region VariableDeclaration
+
+        
+        private int[] array;
+        private int top;
+        private int max;
+        #endregion
+
+        #region constructor
+
+        Mystack(int size)
+        {
+            array = new int[size];
+            top = -1;
+            max = size;
+        }
+        #endregion
+
+        #region method
+
+        public void push(int item)
+        {
+            
+                if (top == max - 1)
+                {
+                    throw new StackException("Stack is Full");
+            }
+                else
+                {
+                    array[++top] = item;
+                }
+            
+            
+            
+            
+
+        }
+        public int pop()
+        {
+            if (top == -1)
+            {
+                throw new StackException("Stack is Empty");
+
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    return array[top--];
+                }
+            }
+            return 0;
+        }
+        public void printStack()
+        {
+            if (top == -1)
+            {
+                throw new StackException("Stack is Empty");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]: " + array[i]);
+                }
+            }
+            #endregion
+
+
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("enter the size of array");
+                int size = int.Parse(Console.ReadLine());
+                Mystack stack = new Mystack(size);
+
+                while (true)
+                {
+                    Console.WriteLine("option : 1.push 2.pop 3.display  4.exit");
+                    int option = int.Parse(Console.ReadLine());
+
+
+
+                    if (option == 1)
+                    {
+                        try
+                        {
+                            Console.WriteLine("enter element");
+                            int element = int.Parse(Console.ReadLine());
+
+                            stack.push(element);
+
+                        }
+                        catch (StackException e) { Console.WriteLine(e); }
+
+                    }
+                    else if (option == 2)
+                    {
+                        try
+                        {
+                            stack.pop();
+                        }
+                        catch (StackException e) { Console.WriteLine(e); }
+
+                    }
+
+                    else if (option == 3)
+                    {
+                        stack.printStack();
+                    }
+                    else if (option == 4)
+                    {
+                        break;
+                    }
+
+                }
+
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Message : "+e.Message);
+            }
+            
+
+            
+
+
+
+        }   
+    }
+}
