764aba21b25f78457cefdbef5f612900a8101e35
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-11-02 23:18:35 +0530

    sum of array elements
    Assignment 1.3

---
+++ SumOfArray/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+
+namespace SumOfArray
+{
+    internal class Program
+    {
+        public class Calculate
+        {
+
+            #region method
+
+            
+            public void addition(int[] array)
+            {
+                int result = 0;
+                foreach (int i in array)
+                {
+                    
+                    result += i;
+
+                }
+                Console.WriteLine("the sum of entered number is " + result);
+
+            }
+            #endregion
+
+            static void Main(string[] args)
+            {
+                try
+                {
+
+                    Calculate c = new Calculate();
+                    Console.WriteLine("How many numbers you want to enter");
+                    int size = int.Parse(Console.ReadLine());
+
+                    int[] array = new int[size];
+                    Console.WriteLine("enter the numbers you want to add");
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        array[i] = int.Parse(Console.ReadLine());
+                    }
+
+                    c.addition(array);
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error Message"+e.Message);
+                }
+                
+
+
+
+
+
+
+            }
+        }
+        
+        
+    }
+}
