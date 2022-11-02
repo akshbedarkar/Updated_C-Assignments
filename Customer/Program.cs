a4d033a23a1dc1862e52a73b88344f998bd67570
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-11-02 22:12:31 +0530

    Calculate Highest Marks
    Assignment 1.2

---
+++ Customer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Customer
+{
+    public class customer
+    {
+        #region
+
+      
+        public int custid { get; set; }
+        public string name { get; set; }
+
+        public string city { get; set; }
+
+        public double balance { get; set; }
+
+        #endregion
+    }
+    class TestCustomer
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<customer> customers = new List<customer>();
+                customer c = new customer { custid = 101, name = "akshata", city = "akola", balance = 5000 };
+                customer c1 = new customer { custid = 102, name = "ak", city = "akola", balance = 5000 };
+                customer c2 = new customer { custid = 103, name = "aksh", city = "akola", balance = 5000 };
+                customer c3 = new customer { custid = 104, name = "akhata", city = "akola", balance = 5000 };
+
+                customers.Add(c);
+                customers.Add(c1);
+                customers.Add(c2);
+                customers.Add(c3);
+
+                foreach (customer obj in customers)
+                {
+                    Console.WriteLine(obj.custid + " " + obj.name + " " + obj.city + " " + obj.balance);
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error Message"+e.Message);
+            }
+
+
+
+
+        }
+    }
+
+}
