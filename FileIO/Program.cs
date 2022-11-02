457a6b0fbdbd74045bf7210b1fe9694e03e14469
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-11-02 22:31:57 +0530

    Basic File Operation
    Assignment 7.1

---
+++ FileIO/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.IO;
+using System.IO;
+
+namespace FileIO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\Users\akshata\source\fieio\MyDirectory\MyAnotherDirectory\NewFile.txt";
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error Message :"+e.Message);
+            }
+            
+
+            
+
+
+
+
+
+
+        }
+    }
+}
